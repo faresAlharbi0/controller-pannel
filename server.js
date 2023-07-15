d34434ccaa73bbb1d2f92d3e589f2a0d479ee1c3
faresAlharbi0 <2040846@uj.edu.sa>
2023-07-15 18:58:14 +0300

    added back end post req/res - fares alharbi

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+app.use(express.json());
+app.use("/",express.static("./website"));
+app.listen(2500,(req,res)=>{
+    console.log("started")
+})
+app.post('/',(req,res)=>{
+    const parcel = req.body
+    if(!parcel){
+        return res.status(400).send({status:'failed'})
+    }
+    res.status(200).send({status:'recieved'})
+    console.log(parcel['parcel']);
+})
--- website/js/index.js
+++ website/js/index.js
@@ -2,21 +2,41 @@ const expandicon = document.getElementById("expandIcon");
 expandicon.addEventListener("click", slideMenu);
 const slider = document.getElementById("sliderpanel");
 const tl = gsap.timeline({
-    reversed: true,
-    paused: true,
-    defaults: {
-      duration: 0.75
-    }
-  });
-  tl.set(slider,{autoAlpha:1})
-  tl.to(slider,{ width: "90%"})
-  function slideMenu() {
-    // play or reverse the timeline
-    tl.reversed() ? tl.play() : tl.reverse();
-    if(!tl.reversed()){
-      expandicon.innerHTML = "<span class='material-symbols-outlined'>keyboard_double_arrow_right</span>";
-    }
-    else{
-      expandicon.innerHTML = " <span class='material-symbols-outlined'>keyboard_double_arrow_left</span>";
-    }
+  reversed: true,
+  paused: true,
+  defaults: {
+    duration: 0.75
   }
+});
+tl.set(slider,{autoAlpha:1})
+tl.to(slider,{ width: "90%"})
+function slideMenu() {
+  // play or reverse the timeline
+  tl.reversed() ? tl.play() : tl.reverse();
+  if(!tl.reversed()){
+    expandicon.innerHTML = "<span class='material-symbols-outlined'>keyboard_double_arrow_right</span>";
+  }
+  else{
+    expandicon.innerHTML = " <span class='material-symbols-outlined'>keyboard_double_arrow_left</span>";
+  }
+}
+const baseUrl ="http://localhost:2500/";
+const Forward = document.getElementById("forward");
+Forward.addEventListener("click",addDirection);
+const Backward = document.getElementById("backward");
+Backward.addEventListener("click",addDirection);
+const Left = document.getElementById("left");
+Left.addEventListener("click",addDirection);
+const Right = document.getElementById("right");
+Right.addEventListener("click",addDirection);
+const Stop = document.getElementById("stop");
+Stop.addEventListener("click",addDirection);
+async function addDirection(e){
+  e.preventDefault();
+  const res = await fetch(baseUrl,
+    {method:'POST',
+    headers:{"Content-Type":'application/json'},
+    body: JSON.stringify({parcel: e.target.id})
+  });
+}
+
