7c93404f1713ab63bca3e960de05331e6e780d6e
faresAlharbi0 <2040846@uj.edu.sa>
2023-07-15 20:45:49 +0300

    added a comment fares

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 app.use("/",express.static("./website"));
 app.listen(2500,(req,res)=>{
-    console.log("started")
+    console.log("started") /**to run server type in the terminal: npm run dev */
 })
 app.post('/',(req,res)=>{
     const parcel = req.body
